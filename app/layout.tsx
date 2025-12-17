3afd400ffb1785a703347e4aa9fef7ea8b055fc2
Anthony Flaherty <aflaherty@certinia.com>
2025-12-17 10:39:46 +0000

    Move pantry h1 to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <h1 className="text-4xl font-bold">pantry</h1>
         {children}
       </body>
     </html>
