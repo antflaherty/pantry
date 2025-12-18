935523ff587f99f5c0fbfc217fa7ccb5861c946d
Anthony Flaherty <antrflaherty@gmail.com>
2025-12-18 16:48:05 +0000

    Add some horizontal padding

--- app/pantry/page.tsx
+++ app/pantry/page.tsx
@@ -2,7 +2,7 @@ import PantryList from "../ui/pantry/pantry-list";
 
 export default async function Page() {
   return (
-    <div className="my-10 max-w-md mx-auto w-full">
+    <div className="my-10 max-w-md mx-auto w-full px-4">
       <PantryList></PantryList>
     </div>
   );
