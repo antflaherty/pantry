adecbce5c7f5a686311e6d0c7afc6e3d7b8e7443
Anthony Flaherty <antrflaherty@gmail.com>
2025-12-18 10:08:52 +0000

    Add signup-form component

---
+++ app/ui/signup-form.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+export default function SignupForm() {
+  const isPending = false;
+  const errorMessage = null;
+
+  return (
+    <form>
+      <div className="flex-1 px-6 pb-4 pt-8">
+        <h1 className={`mb-3 text-2xl`}>sign up</h1>
+        <div className="w-full">
+          <div>
+            <label
+              className="mb-3 mt-5 block text-xs font-medium"
+              htmlFor="email"
+            >
+              email
+            </label>
+            <div className="relative">
+              <input
+                className="peer block w-full rounded-md border border-gray-200 bg-white py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                id="email"
+                type="email"
+                name="email"
+                placeholder="Enter your email address"
+                required
+              />
+            </div>
+          </div>
+          <div className="mt-4">
+            <label
+              className="mb-3 mt-5 block text-xs font-medium"
+              htmlFor="password"
+            >
+              password
+            </label>
+            <div className="relative">
+              <input
+                className="peer block w-full rounded-md border border-gray-200 bg-white py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                id="password"
+                type="password"
+                name="password"
+                placeholder="enter password"
+                required
+                minLength={6}
+              />
+            </div>
+          </div>
+          <div className="mt-4">
+            <label
+              className="mb-3 mt-5 block text-xs font-medium"
+              htmlFor="confirm-password"
+            >
+              confirm password
+            </label>
+            <div className="relative">
+              <input
+                className="peer block w-full rounded-md border border-gray-200 bg-white py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                id="confirm-password"
+                type="password"
+                name="confirm-password"
+                placeholder="confirm password"
+                required
+                minLength={6}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="mt-4 w-full flex justify-center">
+          <button
+            className="flex mt-4 bg-primary h-10 items-center justify-center rounded-lg px-6 text-sm transition-all hover:opacity-80 active:scale-95"
+            aria-disabled={isPending}
+          >
+            sign up
+          </button>
+        </div>
+        <div className="flex h-8 items-end space-x-1">
+          {errorMessage && (
+            <>
+              <p className="text-sm text-red-500">{errorMessage}</p>
+            </>
+          )}
+        </div>
+      </div>
+      <input type="hidden" name="redirectTo" value="/pantry" />
+    </form>
+  );
+}
