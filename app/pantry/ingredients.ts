d67ffc95bf36b34dd8ec93ce513af722a443c573
Anthony Flaherty <antrflaherty@gmail.com>
2025-12-18 16:34:37 +0000

    Convert quantities to numbers

--- app/pantry/ingredients.ts
+++ app/pantry/ingredients.ts
@@ -1,62 +1,62 @@
 const ingredients = [
   {
     name: "onion",
-    quantity: "700",
+    quantity: 700,
     units: "grams",
   },
   {
     name: "garlic",
-    quantity: "50",
+    quantity: 50,
     units: "grams",
   },
   {
     name: "milk",
-    quantity: "50",
+    quantity: 50,
     units: "ml",
   },
   {
     name: "tomato",
-    quantity: "500",
+    quantity: 500,
     units: "grams",
   },
   {
     name: "potato",
-    quantity: "1000",
+    quantity: 1000,
     units: "g",
   },
   {
     name: "carrot",
-    quantity: "300",
+    quantity: 300,
     units: "grams",
   },
   {
     name: "bell pepper",
-    quantity: "2",
+    quantity: 2,
     units: "pieces",
   },
   {
     name: "spinach",
-    quantity: "200",
+    quantity: 200,
     units: "grams",
   },
   {
     name: "chicken breast",
-    quantity: "500",
+    quantity: 500,
     units: "grams",
   },
   {
     name: "rice",
-    quantity: "1000",
+    quantity: 1000,
     units: "g",
   },
   {
     name: "pasta",
-    quantity: "500",
+    quantity: 500,
     units: "grams",
   },
   {
     name: "olive oil",
-    quantity: "500",
+    quantity: 500,
     units: "ml",
   },
 ];
