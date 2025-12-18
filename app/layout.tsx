160e48cb793a0f70ca1f36bc7e90cd2f395b250d
Anthony Flaherty <antrflaherty@gmail.com>
2025-12-18 10:58:21 +0000

    Show sign out button only when logged in

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,32 @@
 import "./globals.css";
 import { cal_sans } from "@/app/ui/fonts";
 
-import { signOut } from "@/auth";
+import { auth, signOut } from "@/auth";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
+  const isLoggedIn = !!session?.user;
+
   return (
     <html lang="en">
       <body className={cal_sans.className}>
         <header className="flex items-start justify-between">
           <p className="text-6xl">pantry</p>
-          <button
-            onClick={async () => {
-              "use server";
-              await signOut({ redirectTo: "/login" });
-            }}
-            className="text-sm text-link hover:underline"
-          >
-            sign out
-          </button>
+          {isLoggedIn && (
+            <button
+              onClick={async () => {
+                "use server";
+                await signOut({ redirectTo: "/login" });
+              }}
+              className="text-sm text-link hover:underline"
+            >
+              sign out
+            </button>
+          )}
         </header>
         <main>{children}</main>
       </body>
