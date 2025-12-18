466342725845c1c398b1a1deb78de038cb1e497a
Anthony Flaherty <antrflaherty@gmail.com>
2025-12-18 11:35:53 +0000

    Add signup server action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,9 @@
 
 import { signIn } from "@/auth";
 import { AuthError } from "next-auth";
+import bcrypt from "bcrypt";
+import { MongoClient } from "mongodb";
+import { z } from "zod";
 
 export async function authenticate(
   _prevState: string | undefined,
@@ -20,4 +23,36 @@ export async function authenticate(
     }
     throw error;
   }
+}
+
+export async function signUp(
+  _prevState: string | undefined,
+  formData: FormData
+) {
+  const parsedCredentials = z
+    .object({ email: z.string().email(), password: z.string().min(6) })
+    .safeParse({
+      email: formData.get("email"),
+      password: formData.get("password"),
+    });
+
+  if (!parsedCredentials.success) {
+    return "Invalid credentials.";
+  }
+
+  const { email, password } = parsedCredentials.data;
+
+  const client = new MongoClient(process.env.MONGODB_URI!);
+  const users = client.db("pantryapp").collection("users");
+  const query = { email };
+  const user = await users.findOne(query);
+
+  if (user) {
+    return "User already exists.";
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  await users.insertOne({ email, password: hashedPassword });
+
+  await signIn("credentials", { email, password, redirectTo: "/pantry" });
 }
