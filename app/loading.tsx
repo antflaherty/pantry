3af5c00820b6fd16af0554a9ca056a223d8974c2
Anthony Flaherty <antrflaherty@gmail.com>
2025-12-19 16:30:40 +0000

    Add root level loading page

---
+++ app/loading.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Loading() {
+  return (
+    <div className="flex h-[50vh] w-full items-center justify-center">
+      <Image
+        src="./icon.svg"
+        width={80}
+        height={80}
+        alt="loading..."
+        className="animate-spin"
+      />
+    </div>
+  );
+}
