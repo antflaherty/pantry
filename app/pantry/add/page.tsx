97f1b8674819212fb643fef4bd47118cd377c3b8
Anthony Flaherty <antrflaherty@gmail.com>
2025-12-21 18:35:13 +0000

    Create add form

--- app/pantry/add/page.tsx
+++ app/pantry/add/page.tsx
@@ -1,9 +1,18 @@
 import { Metadata } from "next";
 
+import { fetchIngredients } from "@/app/lib/data";
+import AddToPantryForm from "@/app/ui/pantry/add/add-to-pantry-form";
+
 export const metadata: Metadata = {
   title: "add to pantry",
 };
 
-export default function Page() {
-  return <></>;
+export default async function Page() {
+  const ingredients = await fetchIngredients();
+
+  return (
+    <div className="my-10 max-w-md mx-auto w-full px-4">
+      <AddToPantryForm ingredients={ingredients} />
+    </div>
+  );
 }
---
+++ app/ui/pantry/add/add-to-pantry-form.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { Ingredient } from "@/app/lib/definitions";
+
+export default function AddToPantryForm({
+  ingredients,
+}: {
+  ingredients: Ingredient[];
+}) {
+  return (
+    <form>
+      <div className="flex-1 px-6 pb-4 pt-8">
+        <h1 className={`mb-3 text-2xl`}>ingredient</h1>
+        <div className="w-full">
+          <div>
+            <label
+              className="mb-3 mt-5 block text-xs font-medium"
+              htmlFor="ingredient"
+            >
+              ingredient
+            </label>
+            <div className="relative">
+              <select
+                id="ingredient"
+                name="ingredientId"
+                className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                defaultValue=""
+                required
+              >
+                <option value="" disabled>
+                  choose an ingredient
+                </option>
+                {ingredients.map((ingredient) => (
+                  <option key={ingredient.id} value={ingredient.id}>
+                    {ingredient.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+          <div className="mt-4">
+            <label
+              className="mb-3 mt-5 block text-xs font-medium"
+              htmlFor="quantity"
+            >
+              quantity
+            </label>
+            <div className="relative">
+              <input
+                className="peer block w-full rounded-md border border-gray-200 bg-white py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                id="quantity"
+                type="quantity"
+                name="quantity"
+                placeholder="enter quantity"
+                required
+                minLength={6}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="mt-4 w-full flex justify-center">
+          <button className="flex mt-4 bg-primary h-10 items-center justify-center rounded-lg px-6 text-sm transition-all hover:opacity-80 active:scale-95">
+            add
+          </button>
+        </div>
+      </div>
+      <input type="hidden" name="redirectTo" value="/pantry" />
+    </form>
+  );
+}
